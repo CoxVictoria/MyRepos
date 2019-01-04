e931238099dc4c6f1fac1da8c00bf71ed5087248
CoxVictoria <46382811+CoxVictoria@users.noreply.github.com>
2019-01-04 10:34:48 -0800

    added files

---
+++ Web/typed/type.js
@@ -0,0 +1,76 @@
+jQuery(document).ready(function() {
+  var type = new Typed('#type', {
+    strings: [
+      'FUN FUN FUN FUN',
+      'FUN FUN FUN FUN',
+      'FUN FUN FUN FUN',
+      'FUN FUN FUN FUN',
+      'LOTS OF FUN FUN',
+      'ISN"T THIS  FUN'
+    ],
+    typeSpeed: 20,
+    backSpeed: 20,
+    backDelay: 15,
+    loop: true,
+  });
+  var title1 = new Typed('#cardTitle1', {
+    strings: [
+      'LittleBig planet',
+    ],
+    typeSpeed: 30,
+    backSpeed: 50,
+    backDelay: 1500,
+    loop: true,
+    cursorChar: ' ',
+  });
+  var title2 = new Typed('#cardTitle2', {
+    strings: [
+      'ROBLOX, with blocks',
+    ],
+    typeSpeed: 30,
+    backSpeed: 50,
+    backDelay: 1500,
+    loop: true,
+    cursorChar: ' ',
+  });
+  var title3 = new Typed('#cardTitle3', {
+    strings: [
+      'Clocks!',
+    ],
+    typeSpeed: 30,
+    backSpeed: 50,
+    backDelay: 1500,
+    loop: true,
+    cursorChar: ' ',
+  });
+  var title4 = new Typed('#cardTitle4', {
+    strings: [
+      'raes restaurant',
+    ],
+    typeSpeed: 30,
+    backSpeed: 50,
+    backDelay: 1500,
+    loop: true,
+    cursorChar: ' ',
+  });
+  var title5 = new Typed('#cardTitle5', {
+    strings: [
+      'Bomb ninja',
+    ],
+    typeSpeed: 30,
+    backSpeed: 50,
+    backDelay: 1500,
+    loop: true,
+    cursorChar: ' ',
+  });
+  var title6 = new Typed('#cardTitle6', {
+    strings: [
+      'MEMES OF POTATOS',
+    ],
+    typeSpeed: 30,
+    backSpeed: 50,
+    backDelay: 1500,
+    loop: true,
+    cursorChar: ' ',
+  });
+});
